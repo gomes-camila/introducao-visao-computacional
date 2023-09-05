14cbde8eff840cada59b2fcc9b52ef7a4acaa099
Camila Gomes <camilagomesbsi@gmail.com>
2023-09-05 18:28:31 -0300

    commit inicial

---
+++ 9.script.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+
+imagem = cv2.imread("cat.jpg")
+
+fonte = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(imagem,'OpenCV',(15,65), fonte,
+2,(255,255,255),2,cv2.LINE_AA)
+
+cv2.imshow("Ponte", imagem)
+cv2.waitKey(0)
